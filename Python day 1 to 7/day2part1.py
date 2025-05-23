f69af5916d6668155777a747bda2dcc2122d4aba
Bobmuso <114597979+Bobmuso@users.noreply.github.com>
2025-05-23 15:00:20 +0200

    diction

---
+++ Python day 1 to 7/day2part1.py
@@ -0,0 +1,22 @@
+#Day 2: 30 Days of python programming
+First_name = 'John'
+Last_name = 'Doe'
+full_name = First_name + ' ' + Last_name
+country = 'USA'
+city = 'New York'
+age = 30
+year = 2023
+is_married = False
+is_true = True
+is_light_on = True
+person_info = {
+    'first_name': First_name,
+    'last_name': Last_name,
+    'country': country,
+    'city': city,
+    'age': age,
+    'year': year,
+    'is_married': is_married,
+    'is_true': is_true,
+    'is_light_on': is_light_on
+} 
