fc1c0610ce5e6459bbe84361ee85df50db7112db
Bobmuso <114597979+Bobmuso@users.noreply.github.com>
2025-10-30 11:22:57 +0000

    day 1 of 30

---
+++ SummerBlitz/Day3/301Python.py
@@ -0,0 +1,47 @@
+#Level 1
+#Activity 1
+import sys
+print('python', sys.version)
+
+#Activity 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+#Activity 3
+print("Bob")
+print("Nyembo")
+print("South Africa")
+print('I am enjoying 30 days of python')
+
+#Activity 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Bob"))
+print(type("Nyembo"))
+print(type("South Africa"))
+
+#Level 2 Done ;<
+
+#Level 3
+#Activity 1
+print('this :1 is a', type(1))
+print('this :1.0 is a', type(1.0))
+print('this :1-1j is a', type(1-1j))
+print('this :Bob is a', type("Bob"))
+print('this :True is a', type(True))
+print('this : [a, b, c] is a', type(['a', 'b', 'c']))
+print('this :(a, b, c) is a', type(('a', 'b', 'c')))
+print('this :{a, b, c} is a', type({'a', 'b', 'c'}))
+print('this :{a : 1} is a', type({'a':1}))
+
+#Activity 2 :Find an Euclidian distance between (2, 3) and (10, 8)
+
+print(((10 - 2)**2 + (8 - 3)**2)**(1/2))
